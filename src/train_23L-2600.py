bee5168475f3db83610a4fe85fb2dd99af359965
Muhammad Daniyal Qureshi <l232600@lhr.nu.edu.pk>
2026-09-09 16:49:47 +0500

    Add normalization - 23L-2600

--- src/train_23L-2600.py
+++ src/train_23L-2600.py
@@ -9,6 +9,7 @@ df = pd.read_csv("data/dataset.csv")
 
 X = df.drop("target", axis=1)
 y = df["target"]
+X = (X - X.mean()) / X.std()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = RandomForestClassifier(n_estimators=100, random_state=42)
